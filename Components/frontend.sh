e5f75d22c8dbc8235eb8ff469133f56398e378a8
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-23 21:54:13 +0530

    testing

--- Components/frontend.sh
+++ Components/frontend.sh
@@ -7,3 +7,25 @@ source Components/common.sh
 echo -n "Intalling Nginx : "
 yum install nginx -y >> /tmp/frontend
 Status $?
+
+echo -n "Enable Nginx : "
+systemctl enable nginx  >> /tmp/frontend
+Status $?
+
+echo -n "Start Nginx : "
+systemctl start nginx >> /tmp/frontend
+Status $?
+
+echo -n "Download the content : "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
+Status $?
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip -o /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -n "Completed"
