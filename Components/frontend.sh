534ae4b5094501eb786ac6feed092820bf31a113
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-23 21:46:28 +0530

    testing

--- Components/frontend.sh
+++ Components/frontend.sh
@@ -5,7 +5,7 @@ set -e
 source Components/common.sh
 
 echo -n "Intalling Nginx : "
-yum install nginx -y << /tmp/frontend
+yum install nginx -y >> /tmp/frontend
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
 else    
