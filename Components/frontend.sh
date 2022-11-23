09b59d0b4cf264920f7b499b1d1e97ba7e660a65
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-23 21:43:02 +0530

    testing

--- Components/frontend.sh
+++ Components/frontend.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e 
+
 source Components/common.sh
 
+echo -n "Intalling Nginx : "
 yum install nginx -y
+if [ $? -eq 0]; then
+    echo -e "\e[32m Success[0m"
+else    
+    echo -e "\e[32m Failed in installing[0m"
+fi
