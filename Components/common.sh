d8c886feff30af33aaeca209e1b23d466bf6b426
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-23 21:48:08 +0530

    testing

--- Components/common.sh
+++ Components/common.sh
@@ -5,3 +5,10 @@ if [ $ID -ne 0 ]; then
     exit 1
 fi
 
+Status() {
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[32m Success \e[0m"
+    else    
+        echo -e "\e[32m Failed in installing \e[0m"
+    fi
+}
--- Components/frontend.sh
+++ Components/frontend.sh
@@ -6,8 +6,4 @@ source Components/common.sh
 
 echo -n "Intalling Nginx : "
 yum install nginx -y >> /tmp/frontend
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-else    
-    echo -e "\e[32m Failed in installing \e[0m"
-fi
+status $?
