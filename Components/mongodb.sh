08aa5507c36247e4492afbcac82ad142bee3e282
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-24 20:58:25 +0530

    Creation of Mongodb

--- Components/mongodb.sh
+++ Components/mongodb.sh
@@ -7,14 +7,14 @@ source Components/common.sh
 Component=mongodb
 
 echo -n "Settingup $Component repository :"
-curl -s -o /etc/yum.repos.d/$Component.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/$Component.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo  >> /tmp/$Component
 Status $?
 
 echo -n "Installing Mongo services :"
 yum install -y mongodb-org >> /tmp/$Component
 Status $?
 
-Sysctl mongodb $? mongodb
+Sysctl mongod $? mongodb
 
 echo -n "Updating listen IP :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
