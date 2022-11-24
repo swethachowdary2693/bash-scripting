2be6f78f1445fc37a10650469e0521e3fab7b153
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-24 21:36:35 +0530

    Creation of Catalogue

--- Components/catalogue.sh
+++ Components/catalogue.sh
@@ -8,19 +8,19 @@ Component=catalogue
 Fuser=roboshop
 
 echo -n "Installing NodeJs : "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - >> /tmp/$Component
+curl -sL https://rpm.nodesource.com/setup_16.x | bash >> /tmp/$Component
 yum install nodejs -y  >> /tmp/$Component
 Status $?
 
 echo -n "Create Application user :"
-id $Fuser || useradd $Fuser
+id $Fuser >> /tmp/$Component|| useradd $Fuser
 Status $?
 
 echo -n "Download the catalogue :"
 curl -s -L -o /tmp/$Component.zip "https://github.com/stans-robot-project/$Component/archive/main.zip" >> /tmp/$Component
 cd /home/$Fuser && unzip -o /tmp/$Component.zip >> /tmp/$Component && mv $Component-main $Component
 
-chown -r $Fuser:$Fuser $Component
+chown -R $Fuser:$Fuser $Component
 cd /home/$Fuser/$Component
 npm install >> /tmp/$Component
 
