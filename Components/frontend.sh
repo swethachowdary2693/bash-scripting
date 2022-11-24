b5ef8c3011925fcfd2fe800afc7582d6b8a2e4fe
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-24 22:17:36 +0530

    Creation of Frontend

--- Components/frontend.sh
+++ Components/frontend.sh
@@ -41,10 +41,10 @@ Status $?
 echo -e "Creation of Frontend : \e[32m Success \e[0m"
 
 echo -n "Updating proxy file with catalogue: "
-for component in catalogue; do 
-sed -i -e '/$component/s/localhost/172.31.17.151/' /etc/nginx/default.d/roboshop.conf  >> /tmp/frontend
+for component in catalogue ; do 
+    sed -i -e '/$component/s/localhost/172.31.17.151/' /etc/nginx/default.d/roboshop.conf  >> /tmp/frontend
+    Status $?
 done
-Status $?
 
 echo -n "Restarting the Nginx"
 systemctl restart $Component
