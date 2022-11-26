62e67294a3a0a74248911d43a24e2080843740fa
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-26 21:02:34 +0530

    Creation of redis

--- Components/redis.sh
+++ Components/redis.sh
@@ -20,5 +20,5 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf >> /tmp/$Component
 Status $?
 
 echo -n "Starting $Component : "
-sysctl $Component $? $Component
+Sysctl $Component $? $Component
 Status $?
