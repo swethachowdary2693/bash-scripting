ea0ffab15eb5f1ab211bb85796eb61705ed0c294
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-27 00:16:24 +0530

    Creation of Payment

--- Components/payments.sh
+++ Components/payments.sh
@@ -16,16 +16,16 @@ DownloadExtract
 
 echo -n "Install the dependencies : "
 cd /home/roboshop/payment 
-pip3 install -r requirements.txt >> $Logfile
+pip3 install -r requirements.txt &>> $Logfile
 Status $?
 
 User_ID=$(id -u roboshop)
 Group_ID=$(id -g roboshop)
 
 echo -n "Updating the ID's in $Component.ini file"
-sed -i -e "/^uid/ c uid=$User_ID" -e "/^uid/ c uid=$Group_ID" $Component.ini >> $Logfile
+sed -i -e "/^uid/ c uid=$User_ID" -e "/^gid/ c gid=$Group_ID" $Component.ini >> $Logfile
 Status $?
 
 Settingsystemd
 
-StartingService
+StartingService 
