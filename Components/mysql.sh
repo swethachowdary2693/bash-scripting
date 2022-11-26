05f0e0905a778240da72f32bef021cc9d30d0d88
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-26 22:49:11 +0530

    Creation of mysql

--- Components/mysql.sh
+++ Components/mysql.sh
@@ -5,12 +5,15 @@ source Components/common.sh
 Component=mysql
 
 echo -n "Setting up mysql Repo : "
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo >> $Logfile
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $Logfile
 Status $?
 
 echo -n "Install mysql : "
-yum install mysql-community-server -y >> $Logfile
+yum install mysql-community-server -y &>> $Logfile
 Status $?
 
 Sysctl mysqld $? 
 
+echo -n "Extracting root password"
+DEFAULT_ROOT_PASSWORD=${grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}'} &>> $Logfile
+Status $?
