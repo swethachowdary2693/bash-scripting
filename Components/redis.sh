c6fe7cf6f5d6d7bb43d444d0591c2a83540d1480
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-26 21:01:17 +0530

    Creation of redis

--- Components/redis.sh
+++ Components/redis.sh
@@ -8,7 +8,7 @@ Component=redis
 
 
 echo -n "Get the repository of redis : "
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/$Component.repo -o /etc/yum.repos.d/$Component.repo >>/tmp/$Component
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/$Component.repo >> /tmp/$Component -o /etc/yum.repos.d/$Component.repo >>/tmp/$Component
 Status $?
 
 echo -n "Installing redis : "
@@ -16,7 +16,7 @@ yum install $Component-6.2.7 -y >> /tmp/$Component
 Status $?
 
 echo -n "Changing the Bind ip to allow all :"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf >> /tmp/$Component
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf >> /tmp/$Component
 Status $?
 
 echo -n "Starting $Component : "
