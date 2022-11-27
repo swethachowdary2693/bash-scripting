33e34e27751c52522b89809a2381f9268e9ac4c0
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-27 21:24:09 +0530

    Testing automatic launch of ec2

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -7,4 +7,4 @@ Security_grp="sg-0ab962e577f534c99"
 echo "The AMI ID is $AMI_ID"
 
 echo -n "Launch EC2 instances : "
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $Security_grp --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$Component}]' | jq >> /tmp/instance
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $Security_grp --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Component}]" | jq >> /tmp/instance
