a6ad69df3d139d946905a3bbc354696761004483
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-27 22:20:46 +0530

    Creating multiple instances

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -2,7 +2,8 @@
 
 set -e 
 
-Component=$1 
+Component=$1
+ZoneId="Z045368932D85CAY0T44S" 
 
 #if $1 input is not given then it throw an error
 if [ -z  "$1" ]; then
@@ -15,10 +16,22 @@ Security_grp="sg-0ab962e577f534c99"
 
 echo "The AMI ID is $AMI_ID"
 
+create_server() {
 echo -n "Launch EC2 instances : "
 IPAddress=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $Security_grp --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Component}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo "Private IP address for created Machine $IPAddress" 
 
 echo "Creating Route53 record : "
 sed -e "s/Private_IP/$IPAddress/" -e "s/Component/$Component/" r53.json > /tmp/route53.json
-aws route53 change-resource-record-sets --hosted-zone-id Z045368932D85CAY0T44S --change-batch file:///tmp/route53.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id $ZoneId --change-batch file:///tmp/route53.json | jq
+
+} 
+
+if [ $1 = "All" ]; then
+    for Component is frontend catalogue cart shipping user mongodb mysql rabbitmq redis payment 
+        Component=$Components
+        create_server
+    done
+else
+    create_server
+fi
