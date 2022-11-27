6e0a66f0783d4366281ab1293cfddd6b90370c08
swetha chowdary <swethachowdary2693@gmail.com>
2022-11-27 21:50:32 +0530

    Testing automatic launch of ec2

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -14,7 +14,7 @@ Security_grp="sg-0ab962e577f534c99"
 echo "The AMI ID is $AMI_ID"
 
 echo -n "Launch EC2 instances : "
-IPAddress = $(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $Security_grp --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Component}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+IPAddress=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $Security_grp --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Component}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo "Private IP address for created Machine $IPAddress" 
 
 echo "Creating Route53 record : "
